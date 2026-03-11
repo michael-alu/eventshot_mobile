fd0bd964c8880a9206e0869ac184d7482e806609
emeraldhycient <igwezehycient86@gmail.com>
2026-03-11 10:51:30 +0000

    feat(auth): add domain (Organizer, AuthRepository) and data (Firebase, Firestore)

---
+++ lib/features/auth/data/datasources/firebase_auth_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+/// Wraps Firebase Auth for organizer sign up / sign in.
+class FirebaseAuthDataSource {
+  FirebaseAuthDataSource({FirebaseAuth? auth}) : _auth = auth ?? FirebaseAuth.instance;
+
+  final FirebaseAuth _auth;
+
+  User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<UserCredential> signUpWithEmail({
+    required String email,
+    required String password,
+  }) {
+    return _auth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<UserCredential> signInWithEmail({
+    required String email,
+    required String password,
+  }) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signOut() => _auth.signOut();
+}
